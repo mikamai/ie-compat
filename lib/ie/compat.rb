fe5bd77955a976979201ccdec67eb914c05a34c6
Elia Schito <elia@schito.me>
2014-04-17 11:45:10 +0200

    Expose assets path

--- lib/ie/compat.rb
+++ lib/ie/compat.rb
@@ -2,6 +2,14 @@ require "ie/compat/version"
 
 module Ie
   module Compat
-    # Your code goes here...
+    IE_COMPAT_ASSETS_PATH = File.expand_path('../../../assets', __FILE__).freeze
+
+    def ie_compat_assets_path
+      IE_COMPAT_ASSETS_PATH
+    end
+
+    def self.assets_path
+      IE_COMPAT_ASSETS_PATH
+    end
   end
 end
